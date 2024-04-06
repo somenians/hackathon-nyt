064ca36f20a1f5fa4a514a88238b1657ae8a13ca
Soems <email>
2024-04-06 15:19:54 -0700

    feat: implemented basic navbar and footer, added fonts

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '$lib/app.css';
+	import Header from '$lib/components/nav/HeaderHome.svelte';
+	import Footer from '$lib/components/nav/Footer.svelte';
+</script>
+
+<div class="min-h-screen relative">
+	<a href="/">
+		<div class="h-16 text-center justify-center">NYT TIMES</div>
+	</a>
+	<Header />
+	<slot />
+	<Footer />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import '$lib/app.css';
+</script>
+
+<div
+	class="flex flex-col h-screen mx-auto max
+    -w-screen-xl items-center lg:py-16 lg:px-12"
+>
+	<h1 class="mb-6 text-3xl md:text-6xl lg:text-5xl">NYT Times</h1>
+</div>
